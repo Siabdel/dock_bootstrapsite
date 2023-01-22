bb99df14212722cbe25c24d4895a26ed7b670698
Django_hp820 <siabdel58@gmail.com>
2023-01-22 08:28:31 +0100

    Error HTTP 400 sur des static files not found modif settings STATIC_ROOT sur BASE_DIR et STATICFILES_DIRS avec PROJECT_DIR

--- app/dock_bootstrapsite/settings.py
+++ app/dock_bootstrapsite/settings.py
@@ -157,12 +157,12 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, "static" )
+STATIC_ROOT = os.path.join(BASE_DIR, "static" )
 # Additional locations of static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static', 'material_bootstrap'),
+    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'static', 'material_bootstrap'),
 ]
 
 MEDIA_URL = '/media/'
